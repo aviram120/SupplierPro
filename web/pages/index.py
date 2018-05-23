11aded89b944fe0f69eeae1b2e9a8681a84e0af1
yaakov300 <yaacov300@gmail.com>
2018-05-23 21:31:25 +0300

    fix bug - add test url to webapp2

--- web/pages/index.py
+++ web/pages/index.py
@@ -10,6 +10,6 @@ class IndexHandler(webapp2.RequestHandler):
         self.response.write(html)
 
 app = webapp2.WSGIApplication([
-    ('/', IndexHandler)
+    ('/test', IndexHandler)
 ], debug=True)
 
