a8a698ffc723d2498aeea08fc66cd97eb77fb0c3
yaakov300 <yaacov300@gmail.com>
2018-05-25 00:19:41 +0300

    add new web tamplate

--- master-Free-Bootstrap-HTML5-Admin-Template/assets/js/dataTables/jquery.dataTables.js
+++ master-Free-Bootstrap-HTML5-Admin-Template/assets/js/dataTables/jquery.dataTables.js
@@ -3791,7 +3791,7 @@
 	
 		/* If the number of columns in the DOM equals the number that we have to
 		 * process in DataTables, then we can use the offsets that are created by
-		 * the web- browser. No custom sizes can be set in order for this to happen,
+		 * the web_v0- browser. No custom sizes can be set in order for this to happen,
 		 * nor scrolling used
 		 */
 		if ( ! userInputs && ! scrollX && ! scrollY &&
@@ -12143,7 +12143,7 @@
 			"bCollapse": null,
 	
 			/**
-			 * Width of the scrollbar for the web-browser's platform. Calculated
+			 * Width of the scrollbar for the web_v0-browser's platform. Calculated
 			 * during table initialisation.
 			 *  @type int
 			 *  @default 0
